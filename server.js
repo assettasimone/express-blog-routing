9a5777f866cc29de8058ba8c0beea83e984ab2e5
assettasimone <assettasimone@gmail.com>
2025-09-09 17:05:55 +0200

    fixed express vulnerability

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Express server inizalization
 const express = require('express')
 const app = express()
 const port = 3000
@@ -8,3 +9,7 @@ app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`)
 })
 
+app.get('/', (req, res) => {
+
+    res.send('First route')
+})
